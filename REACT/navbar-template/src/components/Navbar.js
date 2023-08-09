29c66e2bb35f35d631a1fabd4f5237f058505e47
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-08-09 16:36:46 +0200

    vytvoření struktury + základního JSX,CSS pro Navbar-template

---
+++ REACT/navbar-template/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import "./Navbar.css"
+import logo from "../images/netflix.png"
+import { GiHamburgerMenu } from "react-icons/gi"
+import links from "../data"
+import { useState } from "react"
+
+const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
+
+    return (
+        <nav>
+            <div className="navigation">
+                <div className="nav-header">
+                    <img src={logo} alt="logo" />
+                    <button onClick={() => setShowMenu(!showMenu)}>
+                        <GiHamburgerMenu className="hamburger-icon" />
+                    </button>
+                </div>
+                <div
+                    className={`${
+                        showMenu ? "nav-list show" : "nav-list hide"
+                    }`}
+                >
+                    <ul>
+                        {links.map((oneLink) => {
+                            const { id, text, url } = oneLink
+
+                            return (
+                                <li key={id}>
+                                    <a href={url}>{text}</a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
